76bb0f1c3a0c4a0c7c318d0c76c3c95fdf0bc67d
arkadeepsen <arsen@redhat.com>
2026-03-06 21:07:27 +0530

    Update must-gather path validation
    Signed-off-by: arkadeepsen <arsen@redhat.com>

--- pkg/must-gather/types/common.go
+++ pkg/must-gather/types/common.go
@@ -3,6 +3,6 @@ package types
 // MustGatherParams is a type that contains the must gather path.
 type MustGatherParams struct {
 	// The path to the must gather directory. It must be an absolute path
-	// and contain the must-gather.log file.
+	// and contain the must-gather.logs file.
 	MustGatherPath string `json:"must_gather_path"`
 }

ea004bfe6aacfbfd2a8775b6884d28b9b86bdf9b
arkadeepsen <arsen@redhat.com>
2026-04-01 23:37:00 +0530

    Refactor must-gather layer util functions
    Signed-off-by: arkadeepsen <arsen@redhat.com>

--- pkg/must-gather/utils/utils.go
+++ pkg/must-gather/utils/utils.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/ovn-kubernetes/ovn-kubernetes-mcp/pkg/utils"
 )
 
 // ValidateMustGatherPath validates the must gather path. It will check if the path is not empty,
 // exists, is an absolute path and contains the must-gather.logs file. If the path is not valid,
 // it will return an error.
 func ValidateMustGatherPath(mustGatherPath string) error {
-	if mustGatherPath == "" {
-		return fmt.Errorf("must gather path is required")
-	}
-	if !filepath.IsAbs(mustGatherPath) {
-		return fmt.Errorf("must gather path %s is not an absolute path", mustGatherPath)
+	if err := utils.ValidatePath(mustGatherPath, "must gather path", false); err != nil {
+		return err
 	}
 	mustGatherPath = filepath.Clean(mustGatherPath)
 	if _, err := os.Stat(mustGatherPath); err != nil {
